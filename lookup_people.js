f92893d1cb0a98b32e94e48b8ceffb7d7de621dc
gurpm1991 <gurpm91@gmail.com>
2017-11-21 17:26:23 +0000

    last_name script look-up working

---
+++ lookup_people.js
@@ -0,0 +1,25 @@
+const input = process.argv[2];
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT * FROM famous_people WHERE last_name = $1`, [input], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0]); //output: 1
+    client.end();
+  });
+});
